f46863a2f991b1dce5237f42eb48d61bc10ccbdd
Anurag Mohanty <68963714+AR-Mohanty@users.noreply.github.com>
2024-01-02 15:10:11 +0530

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
-import profilePic from "../assets/images/profile.jpg";
+import profilePic from "../assets/profile.jpg";
 
 const fadeAnimationVariant = {
   initial: {
