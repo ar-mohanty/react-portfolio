ba33765813689364d23ac783b6ae9aba2236c2a5
Anurag Mohanty <68963714+ar-mohanty@users.noreply.github.com>
2024-02-12 19:37:33 +0530

    Styling of logo changes

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -39,7 +39,7 @@ const Contact = () => {
         />
         <div className="isolate bg-transparent px-6 pb-36 lg:px-8">
           <div className="mx-auto max-w-2xl text-center">
-            <img src={} alt="armohanty-logo"/>
+            <img src={logo} alt="armohanty-logo" className="w-56"/>
             <h2 className="font-satoshi text-4xl font-bold tracking-tight text-zinc-300 lg:text-5xl">
               Let's Connect
             </h2>
